5f7694c55b74f0c02c9cde4b74d975a0798ec2ca
Ben Boeckel <ben.boeckel@kitware.com>
2024-06-22 17:02:18 -0400

    persistence: add crate with basic "arc" lookup definition

---
+++ ci-monitor-persistence/src/lib.rs
@@ -0,0 +1,16 @@
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+//! CI monitoring persistence
+//!
+//! Core traits and basic implementations of persistence for CI monitoring.
+
+#![warn(missing_docs)]
+
+mod objects;
+
+pub use self::objects::ArcIndex;
+pub use self::objects::ArcLookup;
