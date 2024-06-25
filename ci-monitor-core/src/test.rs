10f19048bdc6b5876464eb651a10a081ae94a2ec
Ben Boeckel <ben.boeckel@kitware.com>
2024-06-25 02:16:11 -0400

    core: add an impl of `Lookup` for testing purposes

--- ci-monitor-core/src/test.rs
+++ ci-monitor-core/src/test.rs
@@ -4,6 +4,11 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+use std::fmt::Debug;
+use std::sync::Arc;
+
+use crate::Lookup;
+
 macro_rules! assert_missing_field {
     ($err:expr, $type:tt, $field:expr $(,)?) => {
         let in_err = $err;
@@ -19,3 +24,49 @@ macro_rules! assert_missing_field {
     };
 }
 pub(crate) use assert_missing_field;
+
+#[derive(Debug, Default, Clone)]
+pub struct TestLookup {}
+
+/// The "index" of `ArcLookup`.
+///
+/// This is actually just the `Arc` itself.
+pub struct TestIndex<T> {
+    arc: Arc<T>,
+}
+
+impl<T> Clone for TestIndex<T> {
+    fn clone(&self) -> Self {
+        Self {
+            arc: self.arc.clone(),
+        }
+    }
+}
+
+impl<T> Debug for TestIndex<T>
+where
+    T: Debug,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "{:?}", self.arc)
+    }
+}
+
+impl<T> Lookup<T> for TestLookup
+where
+    T: Debug + Send + Sync,
+{
+    /// The type used to lookup instances of `T`.
+    type Index = TestIndex<T>;
+
+    /// Find an instance of `T` given an index.
+    fn lookup<'a>(&'a self, idx: &'a Self::Index) -> Option<&'a T> {
+        Some(&idx.arc)
+    }
+    /// Store an instance of `T` returning an index to get it again.
+    fn store(&mut self, data: T) -> Self::Index {
+        Self::Index {
+            arc: Arc::new(data),
+        }
+    }
+}
