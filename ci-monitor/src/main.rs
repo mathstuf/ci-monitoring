48745de6910a1fb5b78cae0ae70ed7f6c7f7c82b
Ben Boeckel <ben.boeckel@kitware.com>
2024-07-01 15:50:38 -0400

    ci-monitor: construct a simple Gitlab forge instance

--- ci-monitor/src/main.rs
+++ ci-monitor/src/main.rs
@@ -6,21 +6,40 @@
 
 use std::error::Error;
 
-use clap::Command;
+use ci_monitor_gitlab::gitlab;
+use ci_monitor_gitlab::GitlabForge;
+use ci_monitor_persistence::VecLookup;
+use clap::{Arg, ArgAction, Command};
 
 /// A `main` function which supports `try!`.
-fn try_main() -> Result<(), Box<dyn Error>> {
-    let _ = Command::new("ci-monitor")
+async fn try_main() -> Result<(), Box<dyn Error>> {
+    let matches = Command::new("ci-monitor")
         .version(clap::crate_version!())
         .author("Ben Boeckel <ben.boeckel@kitware.com>")
         .about("Monitor CI on a forge to store for further analysis")
+        .arg(
+            Arg::new("TOKEN")
+                .short('t')
+                .long("token")
+                .help("Token to use")
+                .action(ArgAction::Set),
+        )
         .get_matches();
+
+    let token = matches.get_one::<String>("TOKEN").unwrap();
+    let gitlab = gitlab::GitlabBuilder::new("gitlab.kitware.com", token)
+        .build_async()
+        .await
+        .unwrap();
+    let storage = VecLookup::default();
+    let _ = GitlabForge::new("gitlab.kitware.com", gitlab, storage);
 
     Ok(())
 }
 
-fn main() {
-    if let Err(err) = try_main() {
+#[tokio::main]
+async fn main() {
+    if let Err(err) = try_main().await {
         panic!("{:?}", err);
     }
 }
