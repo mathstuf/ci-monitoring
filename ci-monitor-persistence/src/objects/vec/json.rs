5618276c09a5db02ccc7e31b61aa55bf798b1b17
Ben Boeckel <ben.boeckel@kitware.com>
2024-07-25 14:27:33 -0400

    persistence: support JSON storage of `Deployment`

--- ci-monitor-persistence/src/objects/vec/json.rs
+++ ci-monitor-persistence/src/objects/vec/json.rs
@@ -4,11 +4,14 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-use serde::{Deserialize, Serialize};
 use std::any;
 use std::fmt::Debug;
 
-use super::VecStoreError;
+use chrono::{DateTime, Utc};
+use ci_monitor_core::data::{Deployment, DeploymentStatus};
+use serde::{Deserialize, Serialize};
+
+use super::{VecIndex, VecLookup, VecStoreError};
 
 fn invalid_enum_string<T>(value: &str) -> VecStoreError {
     VecStoreError::InvalidEnumString {
@@ -64,4 +67,60 @@ where
 pub(super) trait JsonConvert<T>: for<'a> Deserialize<'a> + Serialize {
     fn convert_to_json(o: &T) -> Self;
     fn create_from_json(&self) -> Result<T, VecStoreError>;
+}
+
+#[derive(Deserialize, Serialize)]
+pub(super) struct DeploymentJson {
+    pipeline: usize,
+    environment: usize,
+    forge_id: u64,
+    created_at: DateTime<Utc>,
+    updated_at: DateTime<Utc>,
+    finished_at: Option<DateTime<Utc>>,
+    status: String,
+
+    cim_fetched_at: DateTime<Utc>,
+    cim_refreshed_at: DateTime<Utc>,
+}
+
+const DEPLOYMENT_STATUS_TABLE: &[(DeploymentStatus, &str)] = &[
+    (DeploymentStatus::Created, "created"),
+    (DeploymentStatus::Running, "running"),
+    (DeploymentStatus::Success, "success"),
+    (DeploymentStatus::Failed, "failed"),
+    (DeploymentStatus::Canceled, "canceled"),
+    (DeploymentStatus::Blocked, "blocked"),
+];
+
+impl JsonConvert<Deployment<VecLookup>> for DeploymentJson {
+    fn convert_to_json(o: &Deployment<VecLookup>) -> Self {
+        Self {
+            pipeline: o.pipeline.idx,
+            environment: o.environment.idx,
+            forge_id: o.forge_id,
+            created_at: o.created_at,
+            updated_at: o.updated_at,
+            finished_at: o.finished_at,
+            status: enum_to_string(DEPLOYMENT_STATUS_TABLE, o.status).into(),
+            cim_fetched_at: o.cim_fetched_at,
+            cim_refreshed_at: o.cim_refreshed_at,
+        }
+    }
+
+    fn create_from_json(&self) -> Result<Deployment<VecLookup>, VecStoreError> {
+        let mut deployment = Deployment::builder()
+            .pipeline(VecIndex::new(self.pipeline))
+            .environment(VecIndex::new(self.environment))
+            .forge_id(self.forge_id)
+            .created_at(self.created_at)
+            .updated_at(self.updated_at)
+            .status(enum_from_string(DEPLOYMENT_STATUS_TABLE, &self.status)?)
+            .build()
+            .unwrap();
+        deployment.finished_at = self.finished_at;
+        deployment.cim_fetched_at = self.cim_fetched_at;
+        deployment.cim_refreshed_at = self.cim_refreshed_at;
+
+        Ok(deployment)
+    }
 }
